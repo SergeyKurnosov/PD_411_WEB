18f0cb699b5d72c18564249a665875db1f0fceb9
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-01-29 20:42:36 +1000

    HWdone

--- JavaScript/DOM/domanimation.js
+++ JavaScript/DOM/domanimation.js
@@ -1,4 +1,3 @@
-// JavaScript source code
 var id = null;
 function Move() {
     const elem = document.getElementById("animation");
@@ -6,25 +5,26 @@ function Move() {
     let y_pos = 0;
     let x_shift = 1;
     let y_shift = 1;
+
     clearInterval(id);
-    // clearInterval(id);
-    //if (flag === true) {
-    //        clearInterval(id);
-    //        flag = true;
-    //    }
     let interval = document.getElementById("interval").value;
+    let font_size = document.getElementById("fontSize").value;
+    elem.style.fontSize = `${font_size}px`;
     id = setInterval(frame, interval);
     function frame() {
-        //if (pos == 250) clearInterval(id);
-        // else {
+        const time = new Date();
+        document.getElementById("hours-unit").innerHTML = time.getHours();
+        document.getElementById("minutes-unit").innerHTML = time.getMinutes();
+        document.getElementById("seconds-unit").innerHTML = time.getSeconds();
+        let with_screen = window.innerWidth;
+        let height_screen = window.innerHeight;
         x_pos += x_shift;
         y_pos += y_shift;
         elem.style.top = x_pos + 'px';
         elem.style.left = y_pos + 'px';
         if (x_pos === 0) x_shift = 1;
-        if (x_pos === 350) x_shift = -1;
+        if (x_pos === height_screen - elem.offsetHeight) x_shift = -1;
         if (y_pos === 0) y_shift = 1;
-        if (y_pos === 250) y_shift = -1;
-        //  }
+        if (y_pos === with_screen - elem.offsetWidth) y_shift = -1;
     }
 }
